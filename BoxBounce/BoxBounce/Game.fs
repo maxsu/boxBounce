90a1b363ba2d68763c349a6b15b83849e1de6845
Max Suica <max.suica@gmail.com>
2021-03-28 23:52:34 -0700

    Update Game.fs

--- BoxBounce/BoxBounce/Game.fs
+++ BoxBounce/BoxBounce/Game.fs
@@ -1,48 +1,74 @@
 ﻿namespace BoxBounce
 
-open System
-open System
 open Rhino
+
+open Rhino.Display.DisplayPipeline
 open Rhino.Input
 open Rhino.Geometry
 open Rhino.Commands
 open GosLib
 open Microsoft.FSharp.Collections
+open System.Diagnostics
+open Rhino.RhinoApp
+
 
 module rs = GosLib.RhinoScriptSyntax
 
+SPACE = 32
+ESC = 27
+RET = 13
+
+
+type keyFactory(hotkey, effect) =
+    hook key = 
+        if key = hotkey 
+        then effect()
+    
+    hookEvent = KeyboardHookEvent hook
+    
+    RhinoApp.add_KeyboardEvent _event
+    
+    member this.Dispose =
+        RhinApp.removeKeyboardEvent hookEvent
+
+type Limiter time  = 
+    let stopWatch = Stopwatch.StartNew()
+
+    member self.limit effect =
+            if stopWatch.EllapsedMilliseconds > time
+            then
+                effect()
+                stopWatch.Restart()
 
 module Game =
-    
-       
+           
     let run (doc:Rhino.RhinoDoc)  =
         
-        let stopWatch = System.Diagnostics.Stopwatch.StartNew()
         let conduit = Core.Conduit()
-
-        //Display.DisplayPipeline.PreDrawObjects.Add conduit.PDO
-        let drawer = Display.DisplayPipeline.PreDrawObjects.Subscribe conduit.PDO
-
-        let addBall i = 
-            if i = 32  && stopWatch.ElapsedMilliseconds > 300L then // spaceaber is 32
-                stopWatch.Restart() 
-                conduit.AddBall(doc.Views.ActiveView.ActiveViewport)
-                doc.Views.Redraw() 
+        use drawer = PreDrawObjects.Subscribe conduit.PDO
+        let view = doc.Views.ActiveView.ActiveViewport()
+        let rate = Limiter 300
         
-        let keyEsc = RhinoApp.KeyboardHookEvent( fun i -> if i = 27 then Core.cancel := true ) // rs.print2 "Key:" i
-        let keySpc = RhinoApp.KeyboardHookEvent addBall // esc=27, enter=13, space = 32  
- 
-        RhinoApp.add_KeyboardEvent keyEsc
-        RhinoApp.add_KeyboardEvent keySpc
+        // Commands
+        let addBall =
+                
+        let addBall = rate.limit(
+            fun () -> 
+                conduit.AddBall(view)
+                doc.Views.Redraw()
+        )
+        
+        let quitGame = Core.cancel := true
+        
+        // Key mappings
+        use keySpc = KeyFactory SPACE addBall
+        use keyEsc = KeyFactory ESC quitGame
 
+
+        // Main loop
         while not !Core.cancel do      
             doc.Views.Redraw()  
             Rhino.RhinoApp.Wait()
 
-        RhinoApp.remove_KeyboardEvent keyEsc
-        RhinoApp.remove_KeyboardEvent keySpc
-
-        drawer.Dispose()
+        // Cleanup
         Core.cancel := false
-        //Display.DisplayPipeline.CalculateBoundingBox.RemoveHandler conduit.CBB
-        //Display.DisplayPipeline.PreDrawObjects.RemoveHandler conduit.PDO
